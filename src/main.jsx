65023cd82ecea474f2962d417790742065c1f16f
phanuel19 <dagbokady@gmail.com>
2025-05-07 13:25:42 +0000

    routes files fixing

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Routes from "./routes/Routes.jsx";
+import Routes from "./Routes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
