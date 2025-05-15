7c6c404895527c5773034d89bb5dd953ba4c7a40
phanuel19 <dagbokady@gmail.com>
2025-05-15 10:54:04 +0000

    Remove unused build and server configuration from Vite setup to streamline project structure

--- vite.config.js
+++ vite.config.js
@@ -6,20 +6,5 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   root: 'src', // Set the root directory to 'src'
- build: {
-    lib: {
-      entry: ['src/main.js'],
-      fileName: (format, entryName) => `${entryName}.${format}.js`,
-      cssFileName: 'index',
-    },
-  },
-  server: {
-    port: 3000, // Development server port
-    open: true, // Automatically open the browser
-  },
-  resolve: {
-    alias: {
-      '@': '/src', // Alias '@' to the 'src' directory for easier imports
-    },
-  },
+ 
 });
