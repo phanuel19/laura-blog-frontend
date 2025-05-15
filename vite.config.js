25572f517f5920666c0fa151924cd9e76c7ee938
phanuel19 <dagbokady@gmail.com>
2025-05-15 10:56:24 +0000

    Refactor Vite configuration for improved clarity and maintainability

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,16 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  root: 'src', // Set the root directory to 'src'
+  root: 'src', // Set the root directory to 'src',
+   build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name == "index.css") return "css/styles.css";
+          return assetInfo.name;
+        },
+      },
+    },
+  },
  
 });
